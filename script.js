c27c03f92316c66cdcf51f89a3c3b075cfae1082
Rascalour <rascalou.raid@gmail.com>
2020-05-19 22:55:04 +0200

    commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// Changement d'image au click
+
+let img = document.getElementById("img_speed")
+function btn1(){
+    img.src = 'images/img_poly.jpg';
+}
+function btn2(){
+    img.src = 'images/img_int.jpg';
+}
+function btn3(){
+    img.src = 'images/im‎g_speed.jpg';
+}
+function btn4(){
+    img.src = 'images/img_share.jpg';
+}
